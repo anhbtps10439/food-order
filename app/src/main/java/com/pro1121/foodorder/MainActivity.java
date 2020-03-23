1afdd4a4f3ee66328531f7d26cbd9aa71774744c
Luân Na <59393413+lunalu153@users.noreply.github.com>
2020-03-24 01:38:00 +0700

    Luân đã đặt chân tới đây

--- app/src/main/java/com/pro1121/foodorder/MainActivity.java
+++ app/src/main/java/com/pro1121/foodorder/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         int i = 0;
-        int i2 = 1;
+        int i2 = 1213214214;
     }
 }
