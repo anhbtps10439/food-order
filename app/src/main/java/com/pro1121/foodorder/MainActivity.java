8e8b6c5d91f2b2464ecd01ac287ed6e49f2344cd
Vilect <kimsusun1234@gmail.com>
2020-03-24 08:50:04 +0700

    Update moi

--- app/src/main/java/com/pro1121/foodorder/MainActivity.java
+++ app/src/main/java/com/pro1121/foodorder/MainActivity.java
@@ -11,11 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        int a = 0;
-        int abc = 2;
-        //testtttt
 
-        String abcxyz = "Hello World";
-        String AAAA = "Helloo";
     }
 }
