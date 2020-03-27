d6ba6a1c1191a44c8095e28c1a658b857d127cb2
trangvnkps08516 <55426902+trangvnkps08516@users.noreply.github.com>
2020-03-27 22:24:31 +0700

    xong phần giao diện log in

---
+++ app/src/main/java/com/pro1121/foodorder/activity/ChangePassWord.java
@@ -0,0 +1,16 @@
+package com.pro1121.foodorder.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pro1121.foodorder.R;
+
+public class ChangePassWord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pass_word);
+    }
+}
---
+++ app/src/main/java/com/pro1121/foodorder/activity/IsYourAccount.java
@@ -0,0 +1,16 @@
+package com.pro1121.foodorder.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pro1121.foodorder.R;
+
+public class IsYourAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_is_your_account);
+    }
+}
