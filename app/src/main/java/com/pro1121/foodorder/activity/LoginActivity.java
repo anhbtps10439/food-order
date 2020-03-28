56e74e22591bde170a251021378c80c9a3a5b8fe
Luân Na <59393413+lunalu153@users.noreply.github.com>
2020-03-29 00:24:07 +0700

    Toolbar replace theo Fragment

--- app/src/main/java/com/pro1121/foodorder/activity/LoginActivity.java
+++ app/src/main/java/com/pro1121/foodorder/activity/LoginActivity.java
@@ -2,11 +2,14 @@ package com.pro1121.foodorder.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.pro1121.foodorder.R;
 
--- app/src/main/java/com/pro1121/foodorder/activity/UserCase/UserCaseActivity.java
+++ app/src/main/java/com/pro1121/foodorder/activity/UserCase/UserCaseActivity.java
@@ -1,5 +1,6 @@
 package com.pro1121.foodorder.activity.UserCase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -7,6 +8,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,35 +18,50 @@ import com.pro1121.foodorder.activity.UserCase.fragment.CategoryUserFragment;
 import com.pro1121.foodorder.activity.UserCase.fragment.HomeUserFragment;
 import com.pro1121.foodorder.activity.UserCase.fragment.ProfileFragment;
 
+
 public class UserCaseActivity extends AppCompatActivity {
 
     private ChipNavigationBar chipNavigationBar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        setTitle("User Case");
         init();
+        hideToolbarTitle();
+
+        //create bottom nav
+        chipNavigationBar.setOnItemSelectedListener(onItemSelectedListener);
+        chipNavigationBar.setItemSelected(R.id.navHomeUser,true);
+    }
+
+
+    private void hideToolbarTitle() {
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("");
     }
 
     private void init() {
         chipNavigationBar = findViewById(R.id.navUserCase);
-        chipNavigationBar.setOnItemSelectedListener(onItemSelectedListener);
-        chipNavigationBar.setItemSelected(R.id.navHomeUser,true);
+        toolbar = findViewById(R.id.toolbarUserCase);
     }
 
     private ChipNavigationBar.OnItemSelectedListener onItemSelectedListener = new ChipNavigationBar.OnItemSelectedListener() {
         @Override
         public void onItemSelected(int i) {
+            FragmentManager fm = getSupportFragmentManager();
             switch (i){
                 case R.id.navHomeUser:
+                    fm.beginTransaction().replace(R.id.nav_host_user_case, new HomeUserFragment()).commit();
                     Toast.makeText(UserCaseActivity.this, "Trang chính", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.navCategoryUser:
+                    fm.beginTransaction().replace(R.id.nav_host_user_case, new CategoryUserFragment()).commit();
                     Toast.makeText(UserCaseActivity.this, "Danh mục", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.navProfile:
+                    fm.beginTransaction().replace(R.id.nav_host_user_case, new ProfileFragment()).commit();
                     Toast.makeText(UserCaseActivity.this, "Người dùng", Toast.LENGTH_SHORT).show();
                     break;
             }
