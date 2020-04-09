15e2cc46d6a3f91cee94b90595eaebf2b3689721
Luân Na <59393413+lunalu153@users.noreply.github.com>
2020-04-09 22:56:43 +0700

    Update dialog add dish kind

--- app/src/main/java/com/pro1121/foodorder/activity/AdminCase/fragment/StoreFragment.java
+++ app/src/main/java/com/pro1121/foodorder/activity/AdminCase/fragment/StoreFragment.java
@@ -1,11 +1,14 @@
 package com.pro1121.foodorder.activity.AdminCase.fragment;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,14 +17,34 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.pro1121.foodorder.R;
+import com.pro1121.foodorder.activity.SignInOut.ChangePassWord;
+import com.pro1121.foodorder.activity.SignInOut.SignInActivity;
+import com.pro1121.foodorder.model.UserModel;
 
-public class StoreFragment extends Fragment {
+public class StoreFragment extends Fragment implements View.OnClickListener {
     private Toolbar toolbar;
+    private TextView tv_greeting, tv_display_name, tv_order_manager, tv_statistic, tv_change_password, tv_sign_out;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getActivity().getWindow().setStatusBarColor(Color.parseColor("#FF3737"));
         View view = inflater.inflate(R.layout.fragment_store_manager,container,false);
         setHasOptionsMenu(true);
+
+        tv_display_name = view.findViewById(R.id.tv_display_name);
+        tv_order_manager = view.findViewById(R.id.tv_order_manager);
+        tv_statistic = view.findViewById(R.id.tv_statistic);
+        tv_change_password = view.findViewById(R.id.tv_change_password);
+        tv_sign_out = view.findViewById(R.id.tv_sign_out);
+
+        tv_order_manager.setOnClickListener(this);
+        tv_statistic.setOnClickListener(this);
+        tv_change_password.setOnClickListener(this);
+        tv_sign_out.setOnClickListener(this);
+
+        UserModel user = SignInActivity.currentUser;
+        tv_display_name.setText(user.getName());
+
         return view;
     }
 
@@ -31,12 +54,36 @@ public class StoreFragment extends Fragment {
         toolbar = getActivity().findViewById(R.id.toolbarAdminCase);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
         toolbar.setTitle("");
+        toolbar.setBackgroundColor(Color.parseColor("#FF3737"));
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_order_manager:
+                //Show tab quản lí order
+                break;
+            case R.id.tv_statistic:
+                //Show tab thống kê
+                break;
+            case R.id.tv_change_password:
+                startActivity(new Intent(getActivity(), ChangePassWord.class));
+                break;
+            case R.id.tv_sign_out:
+                startActivity(new Intent(getActivity(), SignInActivity.class));
+        }
+    }
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         menu.clear();
-        inflater.inflate(R.menu.toolbar_statistic, menu);
+        inflater.inflate(R.menu.toolbar_profile, menu);
+    }
+
+    @Override
+    public void onDestroyView() {
+        toolbar.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        getActivity().getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
+        super.onDestroyView();
     }
 }
--- app/src/main/java/com/pro1121/foodorder/activity/UserCase/fragment/ProfileFragment.java
+++ app/src/main/java/com/pro1121/foodorder/activity/UserCase/fragment/ProfileFragment.java
@@ -1,10 +1,12 @@
 package com.pro1121.foodorder.activity.UserCase.fragment;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,22 +19,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.pro1121.foodorder.R;
+import com.pro1121.foodorder.activity.SignInOut.ChangePassWord;
 import com.pro1121.foodorder.activity.SignInOut.MainActivity;
 import com.pro1121.foodorder.activity.SignInOut.SignInActivity;
 import com.pro1121.foodorder.model.UserModel;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
     private Toolbar toolbar;
-    CircleImageView circleImageView;
-    TextView tv_greeting, tv_display_name, tv_order_history, tv_profile, tv_change_password, tv_sign_out;
+    private CircleImageView circleImageView;
+    private TextView tv_greeting, tv_display_name, tv_order_history, tv_profile, tv_change_password, tv_sign_out;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().getWindow().setStatusBarColor(Color.parseColor("#FF3737"));
         View view = inflater.inflate(R.layout.fragment_profile, container,false);
         setHasOptionsMenu(true);
+
         circleImageView = view.findViewById(R.id.profile_image);
         tv_greeting =view.findViewById(R.id.tv_greeting);
         tv_display_name = view.findViewById(R.id.tv_display_name);
@@ -41,12 +45,32 @@ public class ProfileFragment extends Fragment {
         tv_change_password = view.findViewById(R.id.tv_change_password);
         tv_sign_out = view.findViewById(R.id.tv_sign_out);
 
-        //
+        tv_profile.setOnClickListener(this);
+        tv_order_history.setOnClickListener(this);
+        tv_change_password.setOnClickListener(this);
+        tv_sign_out.setOnClickListener(this);
+
         UserModel user = SignInActivity.currentUser;
         tv_greeting.setText("Xin chào "+user.getName());
         tv_display_name.setText(user.getName());
 
         return view;
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_profile:
+                //Show thông tin cá nhân
+                break;
+            case R.id.tv_order_history:
+                //Show lịch sử order
+                break;
+            case R.id.tv_change_password:
+                startActivity(new Intent(getActivity(), ChangePassWord.class));
+                break;
+            case R.id.tv_sign_out:
+                startActivity(new Intent(getActivity(), SignInActivity.class));
+        }
     }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,7 +87,16 @@ public class ProfileFragment extends Fragment {
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         menu.clear();
-        inflater.inflate(R.menu.toolbar_statistic,menu);
+        inflater.inflate(R.menu.toolbar_profile,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.it_setting_profile:
+                Toast.makeText(getActivity(), "Updating!!!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -72,4 +105,6 @@ public class ProfileFragment extends Fragment {
         getActivity().getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
         super.onDestroyView();
     }
+
+
 }
