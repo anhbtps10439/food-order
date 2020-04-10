eac6916144c17233b76991afd05092ff671c12c2
Vilect <kimsusun1234@gmail.com>
2020-04-10 10:38:15 +0700

    Thêm quyền vào Manifest, yêu cầu cấp quyền runtime.
    Chỉnh sửa code tạo dialog.
    
    Thêm code mở camera và mở gallery

--- app/src/main/java/com/pro1121/foodorder/activity/AdminCase/fragment/CategoryAdminFragment.java
+++ app/src/main/java/com/pro1121/foodorder/activity/AdminCase/fragment/CategoryAdminFragment.java
@@ -1,13 +1,21 @@
 package com.pro1121.foodorder.activity.AdminCase.fragment;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -18,6 +26,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -30,12 +39,38 @@ import com.pro1121.foodorder.dao.DishCategoryDao;
 import com.pro1121.foodorder.dao.DishDao;
 import com.pro1121.foodorder.huyTest.Adapter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class CategoryAdminFragment extends Fragment {
+
+    private Context context;
+
     RecyclerView recyclerView;
     private FloatingActionButton fbCategory;
     private Toolbar toolbar;
     Adapter adapter;
+
+    //chụp ảnh và gallery
+    private Bitmap currrentPhoto;
+    private String photoPath;
+    private Uri photoUri;
+
+    //dialog
+    private ImageView ivCategoryAvatar;
+
+    public static final int REQUEST_CHOOSE_PHOTO_FROM_GALLERY = 2;
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    public CategoryAdminFragment(Context context) {
+        this.context = context;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -54,63 +89,128 @@ public class CategoryAdminFragment extends Fragment {
         fbCategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                final LayoutInflater inflater1 = getActivity().getLayoutInflater();
-                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),R.style.CustomAlertDialog);
-                View view = inflater.inflate(R.layout.dialog_insert_category,null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),R.style.CustomAlertDialog);
+                builder.setView(LayoutInflater.from(context).inflate(R.layout.dialog_insert_category, null, false));
 
-                View dialog_gallery = view.findViewById(R.id.dialog_gallery);
-                ImageView dialog_camera = view.findViewById(R.id.dialog_camera);
-                final EditText dialog_et_dishKind = view.findViewById(R.id.dialog_et_dishKind);
-                final EditText dialog_et_dishKindDes = view.findViewById(R.id.dialog_et_dishKindDes);
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+                ivCategoryAvatar = alertDialog.findViewById(R.id.ivCategoryAvatarDialog);
+                Button btnOpenCam = alertDialog.findViewById(R.id.btnOpenCamDialog);
+                EditText etCategoryCode = alertDialog.findViewById(R.id.etCategoryCodeDialog);
+                EditText etCategoryName = alertDialog.findViewById(R.id.etCategoryNameDialog);
+                EditText etCategoryDes = alertDialog.findViewById(R.id.etCategoryDesDialog);
 
                 //Click to open gallery
-                dialog_gallery.setOnClickListener(new View.OnClickListener() {
+                ivCategoryAvatar.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(getActivity(), "Mở Gallery !!!", Toast.LENGTH_SHORT).show();
+                        //intent mở gallery
+                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                        intent.setType("image/*");
+                        startActivityForResult(intent, REQUEST_CHOOSE_PHOTO_FROM_GALLERY);
                     }
                 });
                 //Click to open Camera
-                dialog_camera.setOnClickListener(new View.OnClickListener() {
+                btnOpenCam.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         //Open Camera
                         Toast.makeText(getActivity(), "Mở Camera !!!", Toast.LENGTH_SHORT).show();
+                        openCam();
                     }
                 });
-
-                builder.setTitle("Thêm loại")
-                        .setView(view)
-                        .setPositiveButton("Thêm", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                                //Get data
-                                String name = dialog_et_dishKind.getText()+"";
-                                String des = dialog_et_dishKindDes.getText()+"";
-
-                                //Insert
-                                DishDao dao = new DishDao(getActivity());
-                                dao.insert("MTL001",name,100100,des,"");
-
-                                adapter.notifyDataSetChanged();
-                                Toast.makeText(getActivity(), "Thêm thành công ", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(getActivity(), "Hủy", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                builder.create();
-                builder.show();
 
             }
         });
 
 
         return view;
+    }
+
+    //hàm lưu file ảnh, đặt tên bằng ngày giờ chụp
+    //File này chưa có data ảnh, chỉ có tên, đường dẫn
+    //hàm này sẽ được gọi để tạo ra một file chứa thống tin trên, sau đó để vào Uri rồi cho vào intent
+    private File createPhotoFile() throws IOException
+    {
+        String takenDate = new SimpleDateFormat("ddMMyyyy_hhmmss_").format(new Date());
+
+        //lấy đường dẫn của riêng app, app khác sẽ không truy cập được
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        //tạo file
+        //các tham số là (tên file, định dạng, đường dẫn)
+        File photoFile = File.createTempFile(takenDate, ".jpg", storageDir);
+
+        //lưu đường dẫn của file
+        photoPath = photoFile.getAbsolutePath();
+
+        return photoFile;
+    }
+
+    private void openCam()
+    {
+        //Khai báo một Intent thực hiện việc gọi một activity chụp ảnh
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // câu lệnh if này để kiểm tra xem hệ thống có Activity nào để chụp ảnh không
+        //vì nếu gọi intent mà không có activity nào hoạt động thì ứng dụng sẽ bị crash
+        if (intent.resolveActivity(getContext().getPackageManager()) != null )
+        {
+            //tạo một file trước
+            File photoFile = null;
+
+            try
+            {
+                //sau khi gọi createPhotoFile, photoPath đã có data đường dẫn
+                photoFile = createPhotoFile();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //nếu photoFile đã được tạo thành công
+            if (photoFile != null)
+            {
+                //gán Uri cho currentPhotoUri
+                photoUri = FileProvider.getUriForFile(getContext(), "com.example.android.fileprovider", photoFile);
+
+                //đặt Uri vào intent
+                //vì action của intent là ACTION_IMAGE_CAPTURE
+                //nên cái extra có key EXTRA_OUTPUT này sẽ giúp camera lưu hình ảnh vào currentPhotoUri
+                //và Intent data ở onActivityResult sẽ null, vì file ảnh đã được lưu trong path rồi
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+
+
+        }
+    }
+
+    //convert bitmap sang byte để upload
+    private byte[] convertToBytes(Bitmap inputBitmap)
+    {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        inputBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    //convert uri sang bitmap để set vào imageview
+    private Bitmap convertToBitmap(Uri inputUri)
+    {
+        try
+        {
+
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), inputUri);
+            return bitmap;
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -128,4 +228,27 @@ public class CategoryAdminFragment extends Fragment {
         inflater.inflate(R.menu.toolbar_category,menu);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK)
+        {
+            switch (requestCode)
+            {
+                case REQUEST_CHOOSE_PHOTO_FROM_GALLERY:
+                {
+                    currrentPhoto = convertToBitmap(data.getData());
+                    ivCategoryAvatar.setImageBitmap(currrentPhoto);
+                }
+
+                case REQUEST_IMAGE_CAPTURE:
+                {
+                    currrentPhoto = convertToBitmap(photoUri);
+                    ivCategoryAvatar.setImageBitmap(currrentPhoto);
+                }
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/pro1121/foodorder/activity/SignInOut/MainActivity.java
+++ app/src/main/java/com/pro1121/foodorder/activity/SignInOut/MainActivity.java
@@ -1,8 +1,16 @@
 package com.pro1121.foodorder.activity.SignInOut;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -17,6 +25,7 @@ import com.pro1121.foodorder.dao.DishCategoryDao;
 import com.pro1121.foodorder.dao.DishDao;
 import com.pro1121.foodorder.dao.OrderDao;
 import com.pro1121.foodorder.dao.UserDao;
+import com.pro1121.foodorder.service.AdminNotifyService;
 
 import java.util.ArrayList;
 
@@ -27,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     DetailOrderDao detailOrderDao;
     DishCategoryDao dishCategoryDao;
     public static ArrayList<Drawable> dishs_img = new ArrayList<>();
+    public static final int REQUEST_PERMISSION_CODE = 5;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +52,36 @@ public class MainActivity extends AppCompatActivity {
         dishCategoryDao = new DishCategoryDao(this);
         dishCategoryDao.getAllRuntime();
         detailOrderDao = new DetailOrderDao(this);
+
+
+        if (permissionCheck(MainActivity.this))
+        {
+            permisssionResquest(MainActivity.this);
+        }
+
+
+//        if (!isServiceRunning(new AdminNotifyService().getClass()))
+//        {
+//            Intent intent = new Intent(MainActivity.this, AdminNotifyService.class);
+//            startService(intent);
+//        }
     }
+
+    private boolean isServiceRunning(Class<?> serviceClass)
+    {
+        ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningServiceInfo serviceInfo:activityManager.getRunningServices(Integer.MAX_VALUE))
+        {
+            if (serviceClass.getName().equals(serviceInfo.getClass().getName()))
+            {
+                return true;
+            }
+        }
+        Toast.makeText(this, "Service is not running!", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
     public void moveToSignUp(View view) {
         if (!LibraryClass.isOnline(this)){
             return;
@@ -53,5 +94,48 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         startActivity(new Intent(this, SignInActivity.class));
+    }
+
+    public static boolean permissionCheck(Context context)
+    {
+        //if permission is not granted
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+    public static void permisssionResquest(Activity activity)
+    {
+
+        ActivityCompat.requestPermissions(activity,
+                new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                REQUEST_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode)
+        {
+            case REQUEST_PERMISSION_CODE: {
+                //if permission is not granted, permission array will be empty
+                if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    //permission granted
+                    Toast.makeText(MainActivity.this, "Permission granted!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
